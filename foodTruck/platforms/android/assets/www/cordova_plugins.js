85deb2021f241c7d99db9ce5ed3cefba0a00dda9
Higor Bocutti <higor_rafael.sp@hotmail.com>
2015-11-12 00:35:28 -0200

    Update2

--- foodTruck/platforms/android/assets/www/cordova_plugins.js
+++ foodTruck/platforms/android/assets/www/cordova_plugins.js
@@ -3,13 +3,11 @@ module.exports = [
     {
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
-        "pluginId": "cordova-plugin-whitelist",
         "runs": true
     },
     {
         "file": "plugins/org.apache.cordova.device-motion/www/Acceleration.js",
         "id": "org.apache.cordova.device-motion.Acceleration",
-        "pluginId": "org.apache.cordova.device-motion",
         "clobbers": [
             "Acceleration"
         ]
@@ -17,7 +15,6 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.device-motion/www/accelerometer.js",
         "id": "org.apache.cordova.device-motion.accelerometer",
-        "pluginId": "org.apache.cordova.device-motion",
         "clobbers": [
             "navigator.accelerometer"
         ]
@@ -25,7 +22,6 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.device-orientation/www/CompassError.js",
         "id": "org.apache.cordova.device-orientation.CompassError",
-        "pluginId": "org.apache.cordova.device-orientation",
         "clobbers": [
             "CompassError"
         ]
@@ -33,7 +29,6 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.device-orientation/www/CompassHeading.js",
         "id": "org.apache.cordova.device-orientation.CompassHeading",
-        "pluginId": "org.apache.cordova.device-orientation",
         "clobbers": [
             "CompassHeading"
         ]
@@ -41,14 +36,33 @@ module.exports = [
     {
         "file": "plugins/org.apache.cordova.device-orientation/www/compass.js",
         "id": "org.apache.cordova.device-orientation.compass",
-        "pluginId": "org.apache.cordova.device-orientation",
         "clobbers": [
             "navigator.compass"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/notification.js",
+        "id": "org.apache.cordova.dialogs.notification",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.dialogs/www/android/notification.js",
+        "id": "org.apache.cordova.dialogs.notification_android",
+        "merges": [
+            "navigator.notification"
         ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
-{}
+{
+    "cordova-plugin-geolocation": "1.0.1",
+    "cordova-plugin-whitelist": "1.0.0",
+    "org.apache.cordova.device-motion": "0.2.11",
+    "org.apache.cordova.device-orientation": "0.3.11",
+    "org.apache.cordova.dialogs": "0.3.0"
+}
 // BOTTOM OF METADATA
 });
